0ce766535a2e25f3186cbbf9ef81ea01e413325f
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2022-07-28 08:59:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendString("smile")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+radio.setGroup(1)
+basic.showNumber(1)
